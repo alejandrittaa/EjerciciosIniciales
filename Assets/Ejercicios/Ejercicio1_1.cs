41920a5f4ff88281aa4533fa7f9899e3cd5fcd14
alejandrittaa <alejandracortesreyero@gmail.com>
2024-09-11 18:17:55 +0200

    ejercicios del apartado 1, en un mismo script

--- Assets/Ejercicios/Ejercicio1_1.cs
+++ Assets/Ejercicios/Ejercicio1_1.cs
@@ -7,12 +7,16 @@ public class Ejercicio1_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("¡Hola Mundo!");
+        Debug.Log("Este es el primer videojuego de Alejandra");
+        Debug.Log("Estoy aprendiendo C#");
+
     }
 
     // Update is called once per frame
+    //Como bien dice el comentario anterior en inglés, al update se le llama una vez por frame, entonces por cada frame que este el juego en activo, se llamara al update y hará que aparezca el mensaje.
     void Update()
     {
-        
+        Debug.Log("Ha pasado un frame");
     }
 }
