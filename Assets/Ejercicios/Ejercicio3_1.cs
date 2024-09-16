e3621f49e6f731f555a2d51ad8dc7a58beb2dcd5
alejandrittaa <alejandracortesreyero@gmail.com>
2024-09-16 11:25:50 +0200

    Ejercicio3_1 y 3_2

---
+++ Assets/Ejercicios/Ejercicio3_1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio3_1 : MonoBehaviour
+{
+
+    [SerializeField] int num1;
+    [SerializeField] int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(num1 + " y " +num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
