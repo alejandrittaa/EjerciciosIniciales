1188671917bb8bab6c376d66f48805960e9d7212
alejandrittaa <alejandracortesreyero@gmail.com>
2024-09-18 16:22:02 +0200

    ejercicio 7_1

---
+++ Assets/Ejercicios/Ejercicio7_1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio7_1 : MonoBehaviour
+{
+
+    int numero = 0;
+
+    void Start()
+    {
+        
+        while (numero < 100)
+        {
+            Debug.Log(numero);
+            numero++;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
