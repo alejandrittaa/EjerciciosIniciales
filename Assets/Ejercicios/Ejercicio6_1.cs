b58cbd7112e3344b72121eeddc64c70766514ab5
alejandrittaa <alejandracortesreyero@gmail.com>
2024-09-28 12:09:50 +0200

    ejercicios 6_1 y 6_2

---
+++ Assets/Ejercicios/Ejercicio6_1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6_1 : MonoBehaviour
+{
+  /*EJERCICIO 6_1 (solo respuestas) (no tengo muy claro que lo haya entendido al 100%, pero si están bien los reseultados entonces si)
+   * (Entiendo que en el momento que hay un OR ||, si hay un true, pues sería TRUE, pero en el momento que hay un AND &&, si hay un false, pues sería false)
+   * 
+   * a) TRUE
+   * b) FALSE
+   * c) TRUE
+   * d) FALSE
+   * e) FALSE
+   * f) TRUE
+  */
+}
---
+++ Assets/Ejercicios/Ejercicio6_2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6_2 : MonoBehaviour
+{
+   /*
+    * 
+    * EJERCICIO 6_2 (solo las respuestas)
+    * 
+    * a) FALSE
+    * b) TRUE
+    * c) TRUE
+    * d) TRUE
+    * e) TRUE
+    * f) FALSE
+    * 
+    * 
+    */
+}
