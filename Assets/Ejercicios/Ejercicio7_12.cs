da1d4745f739f1afca188fbf5427bf13ac5b4fa2
alejandrittaa <alejandracortesreyero@gmail.com>
2024-09-29 11:59:02 +0200

    ejercicio7_12 (no me funciona)

---
+++ Assets/Ejercicios/Ejercicio7_12.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio7_12 : MonoBehaviour
+{
+
+    //NO ENTIENDO PORQUE NO FUNCIONA, Y PORQUE NO SALEN LAS VARIABLES SERIALIZADAS EN UNITY :(
+
+    [SerializeField] int num1;
+    [SerializeField] int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int resultado = SumarTodosLosNumeros(num1, num2);
+        Debug.Log(resultado);
+    }
+
+    private int SumarTodosLosNumeros(int numero1,int numero2)
+    {
+        int suma = 0;
+        if (numero1 > numero2)
+        {
+            for (int i = numero2; i < numero1; i++)
+            {
+                Debug.Log(i);
+                suma += i;
+            }
+            Debug.Log("La suma de todos los número entre " + numero1 + " y " + numero2 + " es: " + suma);
+            return suma;
+        }
+        else if (numero2 < numero1)
+        {
+            for (int i = numero1; i < numero2; i++)
+            {
+                Debug.Log(i);
+                suma += i;
+            }
+            Debug.Log("La suma de todos los número entre " + numero2 + " y " + numero1 + " es: " + suma);
+            return suma;
+        }else
+        {
+            Debug.Log("Introduce dos numero distintos");
+        }
+
+    }
+}
